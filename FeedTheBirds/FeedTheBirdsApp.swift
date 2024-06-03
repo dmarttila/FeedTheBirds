a908c3d2fbcc6d49c35a84763cc22f2cd55119d1
Doug Marttila <dmarttila@gmail.com>
2024-06-03 19:17:19 -0400

    Initial Commit

---
+++ FeedTheBirds/FeedTheBirdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedTheBirdsApp.swift
+//  FeedTheBirds
+//
+//  Created by Douglas Marttila on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct FeedTheBirdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
